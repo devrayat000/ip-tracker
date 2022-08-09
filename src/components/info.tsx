a71ce8907301b173517187f1f1c8576d2c97a770
devrayat000 <2010050@me.buet.ac.bd>
2022-08-09 18:56:28 +0600

    final touch

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -36,19 +36,19 @@ export default Info;
 const InfoCard = styled("ul", {
   backgroundColor: "White",
   round: "1.25rem",
-  //   mx: "1.5rem",
   p: "1.75rem",
   listStyleType: "none",
   display: "grid",
   alignItems: "center",
   rowGap: "1.5rem",
+  boxShadow: "0px 0.5rem 2.5rem $colors$grey",
   $$cgap: 0,
   "@tabletUp": {
     p: "2rem",
     gridTemplateColumns: "repeat(4, 1fr)",
-    alignItems: "start",
-    justifyItems: "start",
-    $$cgap: "3rem",
+    alignItems: "stretch",
+    justifyItems: "stretch",
+    $$cgap: "4rem",
     columnGap: "$$cgap",
   },
 });
@@ -68,6 +68,23 @@ export const InfoDetails = () => {
     </InfoCard>
   );
 };
+
+const ListItem = styled("li", {
+  "@tabletUp": {
+    position: "relative",
+    "&:not(:last-child)::after": {
+      position: "absolute",
+      content: '""',
+      left: "calc(100% + ($$cgap / 2))",
+      color: "red",
+      width: "1px",
+      $$size: "60%",
+      height: "$$size",
+      top: "calc((100% - $$size) / 2)",
+      backgroundColor: "$dark-gray",
+    },
+  },
+});
 
 const InfoTitle = styled("h5", {
   color: "$dark-gray",
@@ -89,9 +106,9 @@ interface InfoItemProps {
 }
 export const InfoItem: React.FC<InfoItemProps> = ({ title, content }) => {
   return (
-    <li>
+    <ListItem>
       <InfoTitle>{title}</InfoTitle>
       <InfoContent>{content}</InfoContent>
-    </li>
+    </ListItem>
   );
 };
